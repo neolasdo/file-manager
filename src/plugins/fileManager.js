991bad53d41260f44874c9773f092bc96ec0d448
Tuan Nguyen <tuan.nd@zinza.com.vn>
2020-05-06 18:01:10 +0700

    update plugin build

--- src/plugins/fileManager.js
+++ src/plugins/fileManager.js
@@ -1,5 +1,16 @@
 import optionsDefaults from "./defaultOpts";
 import FileManager from "../components/FileManager";
+import DetailCard from "../components/DetailCard";
+import FileBreadcrumb from "../components/FileBreadcrumb";
+import FileContextMenu from "../components/FileContextMenu";
+import FileList from "../components/FileList";
+import FilePreviewModal from "../components/FilePreviewModal";
+import FileToolBar from "../components/FileToolBar";
+import FileUploadModal from "../components/FileUploadModal";
+import FolderContextMenu from "../components/FolderContextMenu";
+import FolderList from "../components/FolderList";
+import FormModal from "../components/FormModal";
+import MainContextMenu from "../components/MainContextMenu";
 
 import store from "../store/store";
 
@@ -26,5 +37,16 @@ export default {
     store.$endpoints = options.endpoints
     Vue.prototype.$fileStore = store;
     Vue.component('file-manager', FileManager)
+    Vue.component('detail-card', DetailCard)
+    Vue.component('file-breadcrumb', FileBreadcrumb)
+    Vue.component('file-context-menu', FileContextMenu)
+    Vue.component('file-list', FileList)
+    Vue.component('file-preview-modal', FilePreviewModal)
+    Vue.component('file-tool-bar', FileToolBar)
+    Vue.component('file-upload-modal', FileUploadModal)
+    Vue.component('folder-context-menu', FolderContextMenu)
+    Vue.component('folder-list', FolderList)
+    Vue.component('form-modal', FormModal)
+    Vue.component('main-context-menu', MainContextMenu)
   }
 }
